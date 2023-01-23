7a1ecb023f61eb4ac9de30834827bfa9d77480e5
Sivanujan1998 <ssivanujan1998@gmail.com>
2023-01-23 23:24:26 +0530

    state management App developed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { Store, Select } from '@ngxs/store';
+import { TodoActions } from './state/todo-actions';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { TodoState, ITodo } from './state/todo-state';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-state-management-with-ngxs';
+  title = 'ngxs-todo-app';
+
+  @Select(TodoState) todoList$: Observable<ITodo> | undefined;
+
+  addForm = new FormGroup({
+    title: new FormControl('', [Validators.required])
+  });
+  constructor(private store: Store){}
+  onSubmit(form: any){
+    this.store.dispatch(new TodoActions.AddTodo(form)).subscribe(() => {
+      this.addForm.reset();
+    });
+  }
+  markDone(id: string, is_done: boolean){
+    this.store.dispatch(new TodoActions.markDone(id, is_done));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxsModule } from '@ngxs/store';
+import { environment } from 'src/environments/environment';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TodoState } from './state/todo-state';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule, 
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgbPaginationModule,
+    NgbAlertModule,
+
+    NgxsModule.forRoot([TodoState],{
+      developmentMode: !environment.production
+    }),
+
+    NgxsLoggerPluginModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
